6eda3c6f734b8efa9e49d52b5b089a3c35faff14
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-18 01:26:56 -0500

    model fixes

--- vendor/plugins/marpa_courses_and_lectures/app/models/marpa_course.rb
+++ vendor/plugins/marpa_courses_and_lectures/app/models/marpa_course.rb
@@ -1,4 +1,5 @@
 require "hydra"
+require "marpa/marpa_core"
 
 class MarpaCourse < ActiveFedora::Base
 
@@ -7,35 +8,29 @@ class MarpaCourse < ActiveFedora::Base
     has_relationship "lectures", :is_part_of, :inbound => true
     
     has_metadata :name => "descMetadata", :type => ActiveFedora::QualifiedDublinCoreDatastream do |m|
-      #Teacher dc:creator
-      #Topic  dc:subject
-      #Translator dc:contributor
-      #Format in original recording dc:medium
-      #Our file name dc:alternative
-      #Total length & size of recordings dc:extent      
-      #Languages dc:language
-      #Date of Teaching/ Date Recorded dc:date
-      #Location dc:spatial
+      m.field :tibetan_title, :string, :xml_node => "title", :language => "tibetan"
     end
     
-    has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
-      m.field 'collection', :string
-      m.field 'depositor', :string
-      m.field "sub_topic", :string
-      m.field "commentary_taught", :string 
-      m.field "restriction_level", :string
-      m.field "restriction_instructions", :text
-      m.field "tibetan_materials", :string, :xml_node => "materials", :language => "tibetan"
-      m.field "english_materials", :string, :xml_node => "materials", :language => "english"
-      m.field "originally_recorded_by", :string
-      m.field "contributed_by", :string
-      m.field "notes", :text
-      m.field "original_filename", :text
-      # Location of our master file
-      #number of units in original recording 
-    end
+    has_metadata :name=>"marpaCore", :type=>Marpa::MarpaCore
     
     has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata 
+    
+    # has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
+    #   m.field 'collection', :string
+    #   m.field 'depositor', :string
+    #   m.field "sub_topic", :string
+    #   m.field "commentary_taught", :string 
+    #   m.field "restriction_level", :string
+    #   m.field "restriction_instructions", :text
+    #   m.field "tibetan_materials", :string, :xml_node => "materials", :language => "tibetan"
+    #   m.field "english_materials", :string, :xml_node => "materials", :language => "english"
+    #   m.field "originally_recorded_by", :string
+    #   m.field "contributed_by", :string
+    #   m.field "notes", :text
+    #   m.field "original_filename", :text
+    #   # Location of our master file
+    #   #number of units in original recording 
+    # end
     
     def file_objects_append(file_asset)
       lecture = Lecture.new
--- vendor/plugins/marpa_courses_and_lectures/app/models/marpa_lecture.rb
+++ vendor/plugins/marpa_courses_and_lectures/app/models/marpa_lecture.rb
@@ -5,30 +5,31 @@ class MarpaLecture < ActiveFedora::Base
     include Hydra::ModelMethods
   
     has_relationship "course", :is_part_of
-    has_relationship "file", :is_component_of, :inbound => true
+    has_relationship "file", :is_part_of, :inbound => true
     has_relationship "manifestation", :is_description_of
 
     has_metadata :name => "descMetadata", :type => ActiveFedora::QualifiedDublinCoreDatastream do |m|
-      # Date Recorded dc:date
-      # Topics dc:subject
+      m.field :tibetan_title, :string, :xml_node => "title", :language => "tibetan"
     end
     
-    has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
-      m.field 'collection', :string
-      m.field 'depositor', :string
-      m.field "sub_topic", :string
-      m.field "commentary_taught", :string 
-      m.field "restriction_level", :string
-      m.field "restriction_instructions", :text
-      m.field "tibetan_materials", :string, :xml_node => "materials", :language => "tibetan"
-      m.field "english_materials", :string, :xml_node => "materials", :language => "english"
-      m.field "notes", :text
-      # Location of our master file
-      #number of units in original recording 
-    end
+    has_metadata :name=>"marpaCore", :type=>Marpa::MarpaCore
     
     has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata 
     
+    # has_metadata :name => "properties", :type => ActiveFedora::MetadataDatastream do |m|
+    #   m.field 'collection', :string
+    #   m.field 'depositor', :string
+    #   m.field "sub_topic", :string
+    #   m.field "commentary_taught", :string 
+    #   m.field "restriction_level", :string
+    #   m.field "restriction_instructions", :text
+    #   m.field "tibetan_materials", :string, :xml_node => "materials", :language => "tibetan"
+    #   m.field "english_materials", :string, :xml_node => "materials", :language => "english"
+    #   m.field "notes", :text
+    #   # Location of our master file
+    #   #number of units in original recording 
+    # end
+        
     def file_objects_append(file_asset)
       super(file_asset)
       if relationships[:self][:is_description_of].nil?
