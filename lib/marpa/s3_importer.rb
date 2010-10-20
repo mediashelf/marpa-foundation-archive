5a9fca710e9b4a3ebc2f24c82d417eaf01d867ec
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-10-20 00:50:07 -0500

    starter specs for s3importer

--- lib/marpa/s3_importer.rb
+++ lib/marpa/s3_importer.rb
@@ -4,11 +4,15 @@ require "active-fedora"
 
 class Marpa::S3Importer 
   
-  attr_accessor :bucket_name, :import_directory, :account_id, :import
+  attr_accessor :bucket_name, :bucket, :import_directory, :account_id, :import
   
-  def initialize()
-    @bucket_name = 'marpa-foundation'
-    @import_directory = "import"
+  # Create a new S3Importer
+  # Relies on config/s3.yml to provide S3 access_key_id and secret_access_key
+  # @param [Hash] opts Options: :bucket_name (default: "marpa-foundation""), :import_directory (default: "import")
+  def initialize(opts={})
+    opts = {:bucket_name=>'marpa-foundation', :import_directory=>"import"}.merge(opts)
+    @bucket_name = opts[:bucket_name]
+    @import_directory = opts[:import_directory]
 
     # Set up the connection to S3 if it has not already been established
     s3keys = YAML.load(File.open(File.join(RAILS_ROOT,"config","s3.yml")))
@@ -20,11 +24,13 @@ class Marpa::S3Importer
         :secret_access_key => s3keys["default"]['secret_access_key']
       )
     end
-    account_id = AWS::S3::Base.connection.access_key_id
+    @account_id = AWS::S3::Base.connection.access_key_id
+    @bucket = AWS::S3::Bucket.find(@bucket_name)
   end
   
   def import(import_directory=@import_directory)
     import = load_directory(import_directory)
+    report
     process_import(import)
   end
 
@@ -32,11 +38,33 @@ class Marpa::S3Importer
     @import_directory = import_directory
     puts "loading directory \"#{import_directory}\" from #{@bucket_name} bucket"
     @import = AWS::S3::Bucket.objects(@bucket_name, :prefix=>import_directory)
-    puts "... found #{@import.size} objects in #{import_directory}"
     return @import
   end
+  
+  def report
+    puts "... found #{@import.size} objects in #{import_directory} directory within #{@bucket_name} bucket"
+  end
+  
+  # Create a Fedora object of @model with metadata from @row
+  # @param [AWS::S3::S3Object] s3o the S3 object to import
+  # @return [S3Content] the Fedora S3Content object
+  def fobject_from_s3object(s3object)
+    path = s3object.key.gsub("#{@import_directory}/", "")
+    s3c = S3Content.new
+    s3_ds = s3c.datastreams["s3"]
+      s3_ds.key_values = s3object.key
+      # These methods work, but re-using existing variables for speed.
+      # s3_ds.bucket = obj.bucket.name
+      # s3_ds.account_id = AWS::S3::Base.connection.access_key_id
+      s3_ds.bucket_values = @bucket_name
+      s3_ds.account_id_values = @account_id
+    s3c.datastreams["descMetadata"].import_id_values = path
+    s3c.datastreams["rightsMetadata"].permissions({:group=>"archivist"}, "edit")
+    s3c.datastreams["rightsMetadata"].permissions({:group=>"admin"}, "edit")
+    return s3c
+  end
 
-  def process_import(import=@import)
+  def populate_fedora(import=@import)
     import.each do |obj|
      if obj.key.include?(".DS_Store") 
       puts "tempted to delete #{obj.key}"
@@ -45,20 +73,7 @@ class Marpa::S3Importer
       puts "skipping #{obj.key}"
      else
       puts "importing #{obj.key}"
-      path = obj.key.gsub("#{import_directory}/", "")
-      # fa = FileAsset.new
-      # fa.add_file_datastream(:dsLocation=>obj.url)
-      s3c = S3Content.new
-      s3_ds = s3c.datastreams["s3"]
-        s3_ds.key_values = obj.key
-        # These methods work, but re-using existing variables for speed.
-        # s3_ds.bucket = obj.bucket.name
-        # s3_ds.account_id = AWS::S3::Base.connection.access_key_id
-        s3_ds.bucket_values = @bucket_name
-        s3_ds.account_id_values = @account_id
-      s3c.datastreams["descMetadata"].import_id_values = path
-      s3c.datastreams["rightsMetadata"].permissions({:group=>"archivist"}, "edit")
-      s3c.datastreams["rightsMetadata"].permissions({:group=>"admin"}, "edit")
+      s3c = fobject_from_s3object(obj)
       s3c.save
       puts "... saved as #{s3c.pid}"
      end
---
+++ spec/lib/marpa/s3_importer_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path( File.join( File.dirname(__FILE__), '..','..','spec_helper') )
+
+describe Marpa::S3Importer  do
+
+  before(:all) do
+    @test_importer = Marpa::S3Importer.new
+    @import = @test_importer.load_directory
+    @test_importer.report
+    @bucket = @test_importer.bucket
+    @file1 = @bucket["import/51.mental.factors-bouddha-1988-psc-t-1c/1.mp3"]
+  end
+  
+  describe "fobject_from_s3object" do
+    it "should create an S3Content object with key, bucket & account id " do
+      s3c = @test_importer.fobject_from_s3object(@file1)
+      s3c.should be_kind_of(S3Content)
+      
+      s3_ds = s3c.datastreams["s3"]
+      s3_ds.key_values.should == [@file1.key]
+      s3_ds.bucket_values.should == [@test_importer.bucket_name]
+      s3_ds.account_id_values.should == [@test_importer.account_id]
+    end
+    it "should set permissions on the resulting S3Content object" do
+      s3c = @test_importer.fobject_from_s3object(@file1)
+      s3c.datastreams["rightsMetadata"].permissions({:group=>"archivist"}).should == "edit"
+      s3c.datastreams["rightsMetadata"].permissions({:group=>"admin"}).should == "edit"
+    end
+  end
+  
+end
