d6aa15e1171e8a0c08669fe856d120304148fe3a
mpc3c <mpc3c@d-137-155-223.bootp.Virginia.EDU>
2010-10-26 16:41:27 -0400

    Abstracting some of the UVa-specific ldap stuff and putting it into an initializers file

---
+++ config/initializers/ldap_config.rb
@@ -0,0 +1,12 @@
+# connection settings
+LDAP_HOST = "ldap.virginia.edu"
+LDAP_BASE = "o=University of Virginia,c=US"
+
+# the name of your institution
+LDAP_INSTITUTION = "University of Virginia"
+
+# specific ldap field names
+LDAP_USER_ID = "userid"
+LDAP_FIRST_NAME = "givenname"
+LDAP_LAST_NAME = "sn"
+LDAP_COMPUTING_ID = "uid"
---
+++ lib/ldap/person.rb
@@ -0,0 +1,41 @@
+require 'net/ldap'
+
+module Ldap
+  
+  class Person
+    
+    attr_reader :ldap_vals
+    
+    # create a new Person based on the provided computing ID
+    def initialize(computing_id)
+      ldap = Net::LDAP.new(:host => LDAP_HOST, :base => LDAP_BASE)
+      filter = Net::LDAP::Filter.eq( LDAP_USER_ID, computing_id)
+      attrs = []
+      @vals = {}
+      ldap.search( :base => LDAP_BASE, :attributes => attrs, :filter => filter, :return_result => true ) do |entry|
+        entry.attribute_names.each do |n|
+          @vals[n] = entry[n]
+        end
+      end
+    end
+    
+    def first_name
+      @vals[LDAP_FIRST_NAME.to_sym].first rescue ""
+    end
+    
+    def last_name
+      @vals[LDAP_LAST_NAME.to_sym].first rescue ""
+    end
+    
+    def computing_id
+      @vals[LDAP_COMPUTING_ID.to_sym].first rescue ""
+    end
+    
+    def institution
+      LDAP_INSTITUTION
+    end
+      
+  end
+  
+
+end
