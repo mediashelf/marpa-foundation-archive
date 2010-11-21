2e4c2169f5fb4506fb94b591f2e17cea1731ebb8
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-11-21 17:13:08 -0600

    HYLIBRA-27 - logged in users automatically added to 'registered' group

--- spec/helpers/hydra_access_controls_helper_spec.rb
+++ spec/helpers/hydra_access_controls_helper_spec.rb
@@ -2,6 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe HydraAccessControlsHelper do
   
+  describe "test_permission" do
+    it "should automatically add logged-in users to 'registered' group" do
+      mock_user = mock("User", :login => "logged_in_person")
+      helper.stubs(:read_groups).returns(["registered"])
+      helper.stubs(:current_user).returns(nil)
+      helper.test_permission(:read).should == false 
+      helper.stubs(:current_user).returns(mock_user)
+      helper.test_permission(:read).should == true 
+    end
+  end
+  
   describe "editor?" do
     it "should return true if current_user.is_being_superuser? is true" do
       mock_user = mock("User", :login => "BigWig")
