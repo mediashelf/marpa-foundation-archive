fbccd7d4078b2fead345f5aa47ef4f5eebafcb84
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-11-21 21:59:34 -0600

    release submission errors/notices cleanup

--- app/helpers/libra_helper.rb
+++ app/helpers/libra_helper.rb
@@ -1,16 +1,17 @@
 module LibraHelper
   
   def display_release_status_notice(document)
-    flash[:notice] ||= []
     readiness = document.test_release_readiness
-    if !readiness == true
-      flash[:notice] = flash[:notice] | readiness[:failures]
-    else
+    if readiness == true
+      flash[:notice] ||= []
       if document.submitted_for_release?
         flash[:notice] << "This item has been released for library circulation."
       else
         flash[:notice] << "This item is ready to be released for library circulation."
       end
+    else
+      flash[:error] ||= []
+      flash[:error] = flash[:error] | readiness[:failures]
     end
   end
   
