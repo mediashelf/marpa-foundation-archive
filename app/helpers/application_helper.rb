4b9bf809420fdf952dc6745a744a186d4a06d907
John Scofield <john.scofield@yourmediashelf.com>
2010-11-21 23:05:41 -0500

    HYLIBRA-22: adding title to index view, adding links to images in home view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,15 +15,17 @@ module ApplicationHelper
 
   def render_complex_facet_image(facet_solr_field, item, options = {})
     if File.exists?("#{Rails.root}/public/images/faculty_images/#{extract_computing_id(item.value)}.jpg")
-      "<img src=\"/images/faculty_images/#{extract_computing_id(item.value)}.jpg\" width=\"100\" >"
+      img = "<img src=\"/images/faculty_images/#{extract_computing_id(item.value)}.jpg\" width=\"100\" >"
     else
-      "<img src=\"/images/default_thumbnail.gif\" width=\"90\" >"
+      img = "<img src=\"/images/default_thumbnail.gif\" width=\"90\" >"
     end
+
+    link_to_unless(options[:suppress_link], img, add_facet_params_and_redirect(facet_solr_field, item.value), :class=>"facet_select") 
   end
 
   def get_randomized_display_items items
     if items.length < 9 
-      items
+      items.sort_by {|item| item.value }
     else 
       rdi = items.sort_by {rand}
       return rdi[0..7].sort_by {|item| item.value}
