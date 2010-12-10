eb095d9d6873d0189c2d596e39b02e330b906a6e
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-12-10 03:39:43 -0600

    refining progress box js

--- public/javascripts/jquery.hydraProgressBox.js
+++ public/javascripts/jquery.hydraProgressBox.js
@@ -9,23 +9,35 @@
     * ie. 
     * hydraProgressBox.checkUncheckProgress($(true, 'pbCitationDetails');
     */
-    checkUncheckProgress: function(id, bool) {  
-        el = $("#"+id);
+    checkUncheckProgress: function(elementId, bool) {  
+        el = $("#"+elementId);
         if (bool) {
           el.addClass("progressItemChecked") 
-         // el.show()
+          el.css("background-image",'url(/images/chkbox_checked.png)')
+          // el.show();
         } else {
-          el.removeClass("progressItemChecked")          
+          el.removeClass("progressItemChecked")    
+          el.css("background-image",'url(/images/chkbox_empty.png)')      
         };
-        // $fn.hydraProgressBox.testReleaseReadiness();
+        $.fn.hydraProgressBox.testReleaseReadiness();
+    },
+    
+    showProcessingInProgress: function(elementId) {
+      el = $("#"+elementId);
+      el.css("background-image", 'url(/images/processing.gif)');
+      // el.show();
     },
     
     testReleaseReadiness: function() {
-      // if (citationVerified && abstractProvided && dissertationUploaded && rightsSelected && ccLicenseSelected && copyrightMaterialChecked) {
-      //     $("submitToRegistrar").enable()
-      // } else {
-      //     $("submitToRegistrar").disable()
-      // }
+      var fileUploaded = ($("#file_assets tr.file_asset").length > 0);
+      var titleProvided = ($("#title_info_main_title").attr("value").length > 0);
+      var authorProvided = ($("#person_0_last_name").attr("value").length > 0);
+
+      if (fileUploaded && titleProvided && authorProvided) {
+          $("#submitForRelease").enable();
+      } else {
+          $("#submitForRelease").attr("disabled", "disabled");
+      }
     }
   };
 })( jQuery );
