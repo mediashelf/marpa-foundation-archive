6ede4ab020ec4b3ffe30e04a3fead39a4d3b25df
Garrick Van Buren <garrickvanburen@Garrick-Van-Burens-MacBook-Pro.local>
2011-01-26 16:51:02 -0600

    adding UVAs blue buttons & brown sidebar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,33 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+jQuery(document).ready(function($) {
+	// ACTIVATE THE HOVER STATES OF THE IMAGE LINKS
+	$("img#add-work-image").mouseenter(function(){
+		img_src = $("img#add-work-image").attr('src');
+		$("img#add-work-image").attr('src', img_src.replace(/.png/, "-hover.png"));
+	}).mouseleave(function(){
+		img_src = $("img#add-work-image").attr('src');
+		$("img#add-work-image").attr('src', img_src.replace(/-hover.png/, ".png"));
+	});
+	
+	$("a.learn_more img").mouseenter(function(){
+		img_src = $("a.learn_more img").attr('src');
+		$("a.learn_more img").attr('src', img_src.replace(/.png/, "-hover.png"));
+	}).mouseleave(function(){
+		img_src = $("a.learn_more img").attr('src');
+		$("a.learn_more img").attr('src', img_src.replace(/-hover.png/, ".png"));
+	});
+	
+	// ACTIVATE THE 'ADD YOUR WORK' IMAGE AS A LINK
+	$('img#add-work-image').css('cursor', 'pointer');
+	$('img#add-work-image').click(function() {
+    $('#create-asset-menu').is(":hidden") ? 
+      $('#create-asset-menu').show() : $('#create-asset-menu').hide();
+    });
+});
+
+
 $(function() {
 	// for create asset button at the top
   $("#re-run-action").next().button( {
@@ -17,7 +44,7 @@ $(function() {
 		the_selected_content_type = $('#content_type').val();
 		the_selected_content_type_label = $('#create-asset-menu li[onclick*="' + the_selected_content_type + '"]').html();
 		$("#re-run-action").val(the_selected_content_type_label);
-		$('#re-run-action')[0].onclick = function(){ location.href='/assets/new?content_type=' + the_selected_content_type; };		
+		//$('#re-run-action')[0].onclick = function(){ location.href='/assets/new?content_type=' + the_selected_content_type; };		
 	}
 	
 	
