c1962fe8b021b7d515595ea0f258020fd18f58b9
Garrick Van Buren <garrickvanburen@Garrick-Van-Burens-MacBook-Pro.local>
2011-01-27 21:39:18 -0600

    commenting out js code that makes Fluid play nice with out DatePicker because we're not using Fluid anymore.

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -105,27 +105,27 @@ jQuery(document).ready(function($) {
 	// ADD THE DATEPICKER CLASS TO _DATE FIELDS
 	//	<input type="text" class="datepicker" size="30" name="d1" value="" placeholder="YYYY-MM-DD"/>
 	$('input[name*=_date]').addClass('datepicker');
-	fluid.defaults('inlineEdit').blurHandlerBinder = function(that) {
-		// This is just fluid's normal default handling of a blur event:
-		that.editField.blur(
-			function (evt) {
-				if (that.isEditing())
-					that.finish();
-				return false;
-			}
-		);
+	// fluid.defaults('inlineEdit').blurHandlerBinder = function(that) {
+	// 	// This is just fluid's normal default handling of a blur event:
+	// 	that.editField.blur(
+	// 		function (evt) {
+	// 			if (that.isEditing())
+	// 				that.finish();
+	// 			return false;
+	// 		}
+	// 	);
 		
 		// InlineEdit doesn't watch for change events on the input field, which means that it doesn't
 		// pick up a new date from the datepicker. We fix this by adding a change observer which fires
 		// the same update of the Fluid model that a blur event does.
-		that.editField.change(
-			function (evt) {
-				if (!that.isEditing() && that.editView.value != that.model.value)
-					that.finish();
-				return true;
-			}
-		)
-	}
+	// 	that.editField.change(
+	// 		function (evt) {
+	// 			if (!that.isEditing() && that.editView.value != that.model.value)
+	// 				that.finish();
+	// 			return true;
+	// 		}
+	// 	)
+	// }
 	
   $('a#delete_asset_link').click(
       function () {
