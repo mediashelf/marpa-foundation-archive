4756f3e95121905f790e04a51bb8c835142670e6
Garrick Van Buren <garrickvanburen@Garrick-Van-Burens-MacBook-Pro.local>
2011-03-16 15:07:37 -0500

    removing more mentions of UVA and bringing articles/_edit back into the accordion

--- config/initializers/ldap_config.rb
+++ config/initializers/ldap_config.rb
@@ -3,12 +3,12 @@ LDAP_HOST = "ldap.virginia.edu"
 LDAP_BASE = "o=University of Virginia,c=US"
 
 # the name of your institution
-LDAP_INSTITUTION = "University of Virginia"
+LDAP_INSTITUTION = ""
 
 # specific ldap field names
 LDAP_USER_ID = "userid"
 LDAP_FIRST_NAME = "givenname"
 LDAP_LAST_NAME = "sn"
 LDAP_COMPUTING_ID = "uid"
-LDAP_DEPARTMENT = "uvadisplaydepartment"
+LDAP_DEPARTMENT = "department"
 LDAP_PHOTO = "jpegphoto"
