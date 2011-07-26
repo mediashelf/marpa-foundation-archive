74b5fbeec5f064389787cefe8bbdb30aa593c907
Justin Coyne <jcoyne@justincoyne.com>
2011-07-26 08:57:10 -0500

    Fixed path to datastream

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
-require 'marpa/datastreams/location'
+require 'marpa/datastreams/place'
 class Place < ActiveFedora::Base
-    has_metadata :name => "placeInfo", :type => Marpa::Datastreams::Location
+    has_metadata :name => "placeInfo", :type => Marpa::Datastreams::Place
     has_relationship "course", :is_location_of
 end
