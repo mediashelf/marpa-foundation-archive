31d3d8304222c7a184ba5031d12959685026e146
Justin Coyne <justin.coyne@yourmediashelf.com>
2011-09-15 09:59:52 -0500

    Added blank lines to program drop-down

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe PlacesController do
-  describe "adding a new place" do
+  describe "#new" do
+    it "Should have form" do
+      get :new, :program=>@program
+      response.should be_success
+    end
+  end
+  describe "creating a new place" do
     before do
       @program = Program.new()
       @program.save
+      @count = Place.find(:all, :rows=>1000).size
     end
     it "Should have a create form " do
       post :create, :program=>@program
       response.should redirect_to(edit_program_path(:id=>@program))
       assigns(:place).should_not be_nil
+      Place.find(:all, :rows=>1000).size.should == @count + 1
     end 
 
     after do
