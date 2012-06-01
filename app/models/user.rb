a543b71d6486479bf6bc7cbc84ba28fe633942f1
parallels <parallels@ubuntu.(none)>
2012-06-01 11:46:51 -0500

    reinstalled devise and upgraded db migration for v 2.x

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 # Connects this user object to Hydra behaviors. 
  include Hydra::User
 # Connects this user object to Blacklights Bookmarks and Folders. 
